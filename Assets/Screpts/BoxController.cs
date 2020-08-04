e212fb986641da8e6d5986531b21e870e15d67e0
sergeyunitytest <labindiegame@gmail.com>
2020-08-04 19:34:06 +0300

    Test
    Test

---
+++ Assets/Screpts/BoxController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    public List<GameObject> drop = new List<GameObject>();
+    private SpriteRenderer sr;
+    public Sprite spriteOpen;
+    public GameObject Key;
+    public List<GameObject> doors = new List<GameObject>();
+    private bool Ginerated = false;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player" && !Ginerated)
+        {
+            Ginerated = true;
+            for (int i = 0; i < drop.Count; i++)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+                Instantiate(drop[i], transform.position + randomPos, Quaternion.identity);
+            }
+            for (int i = 0; i < doors.Count; i++)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+                GameObject GeneratedKey = Instantiate(Key, transform.position + randomPos, Quaternion.identity);
+                DoorController Dc = GeneratedKey.GetComponent<DoorController>();
+                Dc.Door = doors[i];
+            }
+            sr.sprite = spriteOpen;
+        }
+    }
+}
---
+++ Assets/Screpts/SpawnerDanje.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class SpawnerDanje : MonoBehaviour
+{
+    public Transform player;
+    public GameObject Enemy;
+    public float SpawnTime = 10f;
+    public List<GameObject> Enemys = new List<GameObject>();
+    public int MaxEnemys = 5;
+    public float Timer = 10f;
+    private float CheckTime = 2f;
+    private float CheckTimer = 2f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        CheckTimer -= Time.deltaTime;
+        if (CheckTimer <= 0f)
+        {
+
+            CheckTimer = CheckTime;
+            for (int i = 0; i < Enemys.Count; i++)
+            {
+                if (/*Enemys.Count < i + 1 &&*/ Enemys[i] == null)
+                {
+                    Enemys.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+        if (Enemys.Count <= MaxEnemys) 
+        {
+            Timer -= Time.deltaTime;
+            if (Timer <= 0f && Vector3.Distance(transform.position, player.position) > 5f)
+            {
+                float x = UnityEngine.Random.Range(-3f, 3f);
+                float y = UnityEngine.Random.Range(-3f, 3f);
+                Vector3 Randompos = new Vector3(transform.position.x + x, transform.position.y + y, 0f);
+                GameObject EnimiSpawned = Instantiate(Enemy, Randompos, Quaternion.identity);
+                Enemys.Add(EnimiSpawned);
+                Timer = SpawnTime;
+            }
+        }
+    }
+}
