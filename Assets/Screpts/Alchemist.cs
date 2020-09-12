d890081cdc179b56591520eae39d097e99edddd6
sergeyunitytest <labindiegame@gmail.com>
2020-09-12 19:33:00 +0300

    Alchemist
    Alchemist

---
+++ Assets/Screpts/Alchemist.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alchemist : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Recipe> recipe = new List<Recipe>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+public class Recipe
+{
+    public List<int> Rets = new List<int>();
+    public int ExitItem;
+    public int CountItem;
+}
